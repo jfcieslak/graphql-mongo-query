4e4f301d66d7a0fb90d57266e4e2a57c287726fb
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2019-11-07 17:15:35 +0100

    fix(types)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { ArgType, Values, Keywords} from './types'
+export type ArgType = 'OPERATOR' | 'COMPUTED' | 'PRIMITIVE' | 'ARRAY' | 'NESTED' | 'FLAT'
+
+export type ValueParser = (parent: any) => any
+
+export interface Values {
+	[key: string]: ValueParser
+}
+
+export interface Keywords {
+	[key: string]: string
+}
 
 export const primitives = [
 	'string',
